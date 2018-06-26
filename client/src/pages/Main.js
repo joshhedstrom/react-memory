87f963ad612f4f03273ce054c9a73a7ce7bdd5af
Josh Hedstrom <josh.hedstrom@gmail.com>
2018-06-26 11:32:54 -0500

    began scaffolding main page

---
+++ client/src/pages/Main.js
@@ -0,0 +1,35 @@
+import React, {Component} from 'react';
+import Container from '../components/Container';
+import Row from '../components/Row';
+import Card from '../components/Card';
+import Footer from '../components/Footer';
+
+class Main extends Component {
+	state = {};
+
+	componentDidMount() {
+
+	}
+
+	handleClick = event => {
+
+	}
+
+
+render(){
+	return(
+		<div>
+			<Container>
+				<Row>
+				</Row>
+				<Row>
+				</Row>
+				<Row>
+				</Row>
+			</Container>
+		</div>
+	)
+}
+
+
+}
