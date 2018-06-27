f37e4815fee13876b3cfd60d0a48f66ebfb06e88
Josh Hedstrom <josh.hedstrom@gmail.com>
2018-06-27 12:23:32 -0500

    added morgan

--- server.js
+++ server.js
@@ -1,12 +1,15 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
+const morgan = require('morgan');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
+
+app.use(morgan('dev'))
 
 if (process.env.NODE_ENV === 'production') {
 	app.use(express.static('client/build'));
